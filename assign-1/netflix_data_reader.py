2845509cc1cc1b95151054d42a01ac04eb08e559
Jan Hauck <janhauck99@gmail.com>
2023-03-29 15:45:42 +0200

    Restructured repo

--- assign-1/netflix_data_reader.py
+++ assign-1/netflix_data_reader.py
@@ -92,6 +92,6 @@ class NetflixReader:
 
 
 reader = NetflixReader()
-reader.read_netflix_data("data/netflix_data.csv")
+reader.read_netflix_data(file_path="assign-1/data/netflix_data.csv")
 reader.preprocess()
-reader.write_netflix_data("output_data")
+reader.write_netflix_data("assign-1/output_data")
