1f2e41810dd1f8c1c4956b4f50954975389504aa
Jan Hauck <janhauck99@gmail.com>
2023-03-29 16:05:11 +0200

    Updated paths + added exercice 2.1

--- assign-1/netflix_data_reader.py
+++ assign-1/netflix_data_reader.py
@@ -89,9 +89,3 @@ class NetflixReader:
 
     def _is_data_leakage(self):
         pass
-
-
-reader = NetflixReader()
-reader.read_netflix_data(file_path="assign-1/data/netflix_data.csv")
-reader.preprocess()
-reader.write_netflix_data("assign-1/output_data")
