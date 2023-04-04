0aa93e90f819b569f7e9a2aa70a5bdc34a2a33de
Jan Hauck <janhauck99@gmail.com>
2023-04-04 19:46:38 +0200

    Renamed run.py to be more specific

--- assign-1/run_netflix_reader.py
+++ assign-1/run_netflix_reader.py
@@ -1,7 +1,12 @@
 from netflix_data_reader import NetflixReader
 
 
+# Add new run configuration to set the correct working directory in order to use these file paths
+# Script Path -> path to run_netflix_reader.py
+# Working Directory -> ml-and-ai-course
 reader = NetflixReader()
 reader.read_netflix_data(file_path="assign-1/data/netflix_data.csv")
 reader.preprocess()
 reader.write_netflix_data("assign-1/output_data")
+
+
