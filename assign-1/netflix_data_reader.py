90107efef9857e55dc40f49bc56c479ecd333d39
Jan Hauck <janhauck99@gmail.com>
2023-04-07 09:50:33 +0200

    Added missing type hinting

--- assign-1/netflix_data_reader.py
+++ assign-1/netflix_data_reader.py
@@ -8,17 +8,17 @@ pd.set_option('display.max_columns', None)
 
 
 class NetflixReader:
-    def __init__(self):
+    def __init__(self) -> None:
         # Create empty pandas DataFrames to hold the
         # Netflix dataset, train data, validation data, and test data
-        self.netflix_data_raw = pd.DataFrame()
-        self.netflix_data = pd.DataFrame()
-        self.train_data = pd.DataFrame()
-        self.val_data = pd.DataFrame()
-        self.test_data = pd.DataFrame()
+        self.netflix_data_raw: pd.DataFrame = pd.DataFrame()
+        self.netflix_data: pd.DataFrame = pd.DataFrame()
+        self.train_data: pd.DataFrame = pd.DataFrame()
+        self.val_data: pd.DataFrame = pd.DataFrame()
+        self.test_data: pd.DataFrame = pd.DataFrame()
         # Create a dictionary that maps column names to
         # data types for the Netflix dataset
-        self._type_dict = {
+        self._type_dict: dict = {
             "index": int,
             "id": "string",
             "title": "string",
@@ -36,7 +36,7 @@ class NetflixReader:
         self._data_split_ratios: dict = {"train": 0.8, "val": 0.2, "test": 0.0}
         self.data_leakage_warning: Tuple[bool, Optional[pd.DataFrame]] = (False, None)
 
-    def set_data_split_ratio(self, new_split_ratio: dict):
+    def set_data_split_ratio(self, new_split_ratio: dict) -> None:
         # Check that the input is a dictionary
         if not isinstance(new_split_ratio, dict):
             raise TypeError('Input must be a dictionary.')
@@ -52,16 +52,13 @@ class NetflixReader:
         if not sum(new_split_ratio.values()) == 1:
             raise ValueError('Values in the input dictionary must add up to 1.')
 
-    def read_netflix_data(self, file_path: str):
+    def read_netflix_data(self, file_path: str) -> None:
         # Reads the Netflix data from the specified file_path using pandas' read_csv method,
         # and saves it to the netflix_data_raw attribute of the NetflixData class.
         # The delimiter is set to ">" because that is the delimiter used in the data file.
         self.netflix_data_raw = pd.read_csv(file_path, delimiter=">")
 
-        # Returns the raw Netflix data
-        return self.netflix_data_raw
-
-    def preprocess(self):
+    def preprocess(self) -> None:
         self.netflix_data_raw.drop(columns="seasons", inplace=True)
         self.netflix_data_raw.drop(columns="age_certification", inplace=True)
         self._drop_missing_values()
@@ -85,21 +82,21 @@ class NetflixReader:
 
         # Write the train split as a .pickle file
         # join root directory with file name to enure that the file is written to the correct location
-        train_file = os.path.join(file_path, "train.pickle")
+        train_file: str = os.path.join(file_path, "train.pickle")
         with open(train_file, "wb") as f:
             pd.to_pickle(self.train_data, f)
 
         # Write the val split as a .pickle file
-        val_file = os.path.join(file_path, "val.pickle")
+        val_file: str = os.path.join(file_path, "val.pickle")
         with open(val_file, "wb") as f:
             pd.to_pickle(self.val_data, f)
 
         # Write the test split as a .pickle file
-        test_file = os.path.join(file_path, "test.pickle")
+        test_file: str = os.path.join(file_path, "test.pickle")
         with open(test_file, "wb") as f:
             pd.to_pickle(self.test_data, f)
 
-    def _drop_missing_values(self):
+    def _drop_missing_values(self) -> None:
         # Drop rows with missing values (i.e., NaN values)
         self.netflix_data_raw.dropna(inplace=True)
 
@@ -113,13 +110,13 @@ class NetflixReader:
 
     # Method not needed. Implemented in next method.
     @staticmethod
-    def _convert_string_to_list(str_list: str):
+    def _convert_string_to_list(str_list: str) -> list:
         pass
 
-    def _convert_list_to_bool(self, column_to_distribute: str):
+    def _convert_list_to_bool(self, column_to_distribute: str) -> None:
         # Extract the list of genres from the specified column,
         # remove unwanted characters, and split them into separate strings
-        genres = self.netflix_data[column_to_distribute].str.strip("[]").str.replace("'", "").str.split(", ")
+        genres: pd.Series = self.netflix_data[column_to_distribute].str.strip("[]").str.replace("'", "").str.split(", ")
         # Loop over each genre in the list of genres
         for genre in set(genre for genres_list in genres for genre in genres_list if genre):  # => `if genre` to check if string not empty
             # Column name = lowercase version of current genre with spaces replaced by '_'
@@ -127,9 +124,9 @@ class NetflixReader:
             # and store the result as a boolean value in the new column
             self.netflix_data[f"{genre.lower().replace(' ', '_')}"] = genres.apply(lambda x: genre in x)  # .apply() -> applies a funciton along a given axis
 
-    def _split_data(self):
+    def _split_data(self) -> None:
         # Create a copy of the Netflix dataset
-        netflix_data_copy = self.netflix_data.copy()
+        netflix_data_copy: pd.DataFrame = self.netflix_data.copy()
 
         # Shuffle the data randomly
         netflix_data_copy = netflix_data_copy.sample(frac=1)
